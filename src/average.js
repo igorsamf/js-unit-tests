64fe21ce6319449326f36cd14fc9e903ed29d30c
Igor Sampaio <igorsamf@gmail.com>
2022-04-09 15:28:49 -0300

    ate o requisito 4

--- src/average.js
+++ src/average.js
@@ -12,6 +12,20 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+const average = (array) => {
+  if (array.length === 0) {
+    return undefined;
+  }
+  let soma = 0
+  for (index = 0; index < array.length; index += 1) {
+    if(typeof array[index] !== 'number') {
+      return undefined;
+    }
+    soma += array[index];
+  }
+return Math.round(soma / array.length);
+
+};
+average([1, 2, 3])
 
 module.exports = average;
